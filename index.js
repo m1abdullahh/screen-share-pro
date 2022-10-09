5e4a3a4dd553bf9585f568aecb34da2be592985b
AbdullahJr69 <akjee204@gmail.com>
2022-10-10 01:21:46 +0500

    added the platform check changed 2 tab spaces to 4

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-const { Server } = require('socket.io')
-const https = require('https');
-const fs = require('fs');
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const https = require("https");
+const fs = require("fs");
 
+console.log(process.platform);
+console.log(process.connected);
 console.log(process.platform);
 // Settings for secure https server
 const options = {
-    key: fs.readFileSync('./cert/cert.key'),
-    cert: fs.readFileSync('./cert/cert.crt')
-}
-const httpsServer = https.createServer(options, app)
+    key: fs.readFileSync("./cert/cert.key"),
+    cert: fs.readFileSync("./cert/cert.crt"),
+};
+const httpsServer = https.createServer(options, app);
 const httpsServerIO = new Server(httpsServer);
 
-const io = new Server(http)
+const io = new Server(http);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/static/'))
+app.use(express.static(__dirname + "/static/"));
 
-io.on('connection', (socket) => {
-    socket.on('signal', (data) => {
-        socket.broadcast.emit('signal', data)
-    })
-})
-httpsServerIO.on('connection', (socket) => {
-    socket.on('signal', (data) => {
-        socket.broadcast.emit('signal', data)
-    })
-})
+io.on("connection", (socket) => {
+    socket.on("signal", (data) => {
+        socket.broadcast.emit("signal", data);
+    });
+});
+httpsServerIO.on("connection", (socket) => {
+    socket.on("signal", (data) => {
+        socket.broadcast.emit("signal", data);
+    });
+});
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/index.html')
-})
+app.get("/", (req, res) => {
+    res.status(200).sendFile(__dirname + "/index.html");
+});
 
 http.listen(PORT, () => {
-    console.log(`Server is live on http://localhost:3000`)
-})
+    console.log(`Server is live on http://localhost:3000`);
+});
 
 // httpsServer.listen(443, () => {
 //     // `Secure HTTPS Sever is live on https://localhost:3000/`
